eaceb8a331055f4290c1600ba3e5410ce0699c46
Nikol Genkovska <2326139g@student.gla.ac.uk>
2019-01-24 17:14:31 +0000

    up to chapter 6

--- rango/urls.py
+++ rango/urls.py
@@ -3,6 +3,8 @@ from rango import views
 urlpatterns = [
 url(r'^$', views.index, name='index'),
 url(r'^about/$', views.about, name='about'),
+url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
+views.show_category, name='show_category'),
 ]
 
 """Map this view to /rango/about/. For this step, you’ll only need to edit the urls.py
--- rango/views.py
+++ rango/views.py
@@ -1,14 +1,39 @@
 from django.shortcuts import render
-
+from rango.models import Category, Page
 from django.http import HttpResponse
 def index(request):
-# Construct a dictionary to pass to the template engine as its context.
-# Note the key boldmessage is the same as {{ boldmessage }} in the template!
-	context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
-# Return a rendered response to send to the client.
-# We make use of the shortcut function to make our lives easier.
-# Note that the first parameter is the template we wish to use.
+	category_list = Category.objects.order_by('-likes')[:5]
+	page_list = Page.objects.order_by('-views')[:5]
+	context_dict = {'categories': category_list,'pages': page_list}
 	return render(request, 'rango/index.html', context=context_dict)
 	#return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/about/'>About</a>")
 def about(request):
 	return render(request, 'rango/about.html')
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+        context_dict['query'] = category.name
+    except Category.DoesNotExist:
+
+        context_dict['category'] = None
+        context_dict['pages'] = None
+    result_list = []
+
+    if request.method == 'POST':
+        query = request.POST['query'].strip()
+        if query:
+            result_list = run_query(query)
+            context_dict['query'] = query
+            context_dict['result_list'] = result_list
+    return render(request, 'rango/category.html', context_dict)
+
+
+
+
