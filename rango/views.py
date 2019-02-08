cd2a64904bafb0f05b98d78cdde9e1bc09421c39
Nikol Genkovska <2326139g@student.gla.ac.uk>
2019-02-08 14:56:58 +0000

    chapter 7

--- rango/views.py
+++ rango/views.py
@@ -11,14 +11,7 @@ from datetime import datetime
 
 
 
-"""def index(request):
-	request.session.set_test_cookie()
-	category_list = Category.objects.order_by('-likes')[:5]
-	page_list = Page.objects.order_by('-views')[:5]
-	context_dict = {'categories': category_list,'pages': page_list}
-	return render(request, 'rango/index.html', context=context_dict)
-	#return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/about/'>About</a>")
-	"""
+
 def index(request):
 	request.session.set_test_cookie()
 	category_list = Category.objects.order_by('-likes')[:5]
